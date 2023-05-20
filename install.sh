ceacbe5172b02d9b0ff7ace88c373aa474ec03be
Paulo Gobetti <78759327+paulogobetti@users.noreply.github.com>
2023-05-20 14:51:09 -0300

    New

--- install.sh
+++ install.sh
@@ -8,9 +8,24 @@
 
 # = = = = = = = = = = = = = = S C R I P T = = = = = = = = = = = = = = #
 
+# Adicionar condicional, se su retornar verdadeiro, continuar scrip, se retornar falso, fechar bash.
+
 su
 
 rm /var/lib/dpkg/lock-frontend; rm /var/cache/apt/archives/lock;
 
 apt update && apt upgrade
+
+apt install sudo ufw git docker.io docker-compose -y
+
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo ufw allow 22
+
+sudo ufw enable
+
+# Instalar imagens Docker
+# docker pull php:8.1.19-apache-bullseye
+
 
