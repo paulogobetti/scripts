ec3f1aef8b9709cfb2aed940fddb5d317e0d86fd
Paulo Gobetti <78759327+paulogobetti@users.noreply.github.com>
2023-05-22 11:09:53 -0300

    Comentários Debian Script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 # = = = = = = = = = = = = = = V A R I Á V E I S = = = = = = = = = = = = = = #
 
-# APPS_APT_INSTALL=( virtualbox )
+APPS_APT_INSTALL=( ufw git sudo docker.io docker-compose )
 
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+# APPS_DOCKER_INSTALL=( virtualbox )
 
 # = = = = = = = = = = = = = = S C R I P T = = = = = = = = = = = = = = #
 
@@ -18,6 +18,10 @@ apt update && apt upgrade
 
 apt install sudo ufw git docker.io docker-compose -y
 
+# Verificar range de IP.
+
+# Setar IP estático (usar algum valor que fica no meio do range disponível).
+
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
@@ -25,7 +29,8 @@ sudo ufw allow 22
 
 sudo ufw enable
 
-# Instalar imagens Docker
+# Instalar imagens Docker.
+
 # docker pull php:8.1.19-apache-bullseye
 
 
