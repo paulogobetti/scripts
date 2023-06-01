d03def16ab4ae94c95e65b0eda94b6c86e2d0af4
Paulo Gobetti <78759327+paulogobetti@users.noreply.github.com>
2023-06-01 18:30:30 -0300

    Up

---
+++ sudo2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# wget https://raw.githubusercontent.com/paulogobetti/this-is-not-a-repository/main/sudo.sh && chmod +x sudo.sh && ./sudo.sh
+
+rm /var/lib/dpkg/lock-frontend
+rm /var/cache/apt/archives/lock
+
+apt install sudo -y & sudo usermod -aG sudo $USER
+
+apt update && apt dist-upgrade -y
+apt autoclean
+apt autoremove -y
+
