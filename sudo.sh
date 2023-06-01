e7fab4f453278122a0fe03a8c5823cd25140984b
Paulo Gobetti <78759327+paulogobetti@users.noreply.github.com>
2023-06-01 18:43:06 -0300

    Update

--- sudo.sh
+++ sudo.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-
+#
 # wget https://raw.githubusercontent.com/paulogobetti/this-is-not-a-repository/main/sudo.sh && chmod +x sudo.sh && ./sudo.sh
+#
+# # # #
 
 rm /var/lib/dpkg/lock-frontend
 rm /var/cache/apt/archives/lock
--- teste.sh
+++ teste.sh
@@ -6,15 +6,14 @@
 #
 
 # apt install sudo wget
-# wget https://raw.githubusercontent.com/paulogobetti/this-is-not-a-repository/main/teste.sh && chmod +x teste.sh && sudo teste.sh
+# wget https://raw.githubusercontent.com/paulogobetti/this-is-not-a-repository/main/teste.sh && chmod +x teste.sh && teste.sh
 
-sudo rm /var/lib/dpkg/lock-frontend; sudo rm /var/cache/apt/archives/lock;
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install ufw ssh openssh-client curl git docker.io docker-compose apt-utils php-xml libc6 libstdc++6 python3-minimal ca-certificates tar openssh-server bash wget -y
-
-sudo usermod -aG docker $USER
+sudo apt install ufw ssh openssh-client curl git docker.io docker-compose apt-utils php-xml libc6 libstdc++6 python3-minimal ca-certificates tar openssh-server bash wget -y && sudo usermod -aG docker $USER
 
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
@@ -24,7 +23,7 @@ sudo ufw enable -y
 
 sudo systemctl enable docker
 
-sudo docker run -d --name vscode -p 8443:8443 -v /home/$USER/.git:/.git -e DEFAULT_WORKSPACE=/.git linuxserver/code-server:latest
+docker run -d --name vscode -p 8443:8443 -v /home/$USER/.git:/.git -e DEFAULT_WORKSPACE=/.git linuxserver/code-server:latest
 
 sudo chown $USER:$USER /home/$USER/.git -R
 
