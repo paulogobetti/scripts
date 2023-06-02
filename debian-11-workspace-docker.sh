84aaef6308a8f64a680670bbd29f3855738ea354
Paulo Gobetti <78759327+paulogobetti@users.noreply.github.com>
2023-06-01 22:03:22 -0300

    Update

--- debian-11-workspace-docker.sh
+++ debian-11-workspace-docker.sh
@@ -3,8 +3,10 @@
 # wget https://raw.githubusercontent.com/paulogobetti/this-is-not-a-repository/main/debian-11-workspace-docker.sh -O /home/$USER/install.sh && chmod +x install.sh && ./install.sh
 
 # Setar variáveis.
+
 LOCAL_IP="$(hostname -I | cut -f1 -d' ')"
-APT_APPS=(ufw ssh openssh-client curl git docker.io docker-compose apt-utils php-xml libc6 libstdc++6 python3-minimal ca-certificates tar openssh-server bash wget)
+USER_PASS="$(date | base64)"
+APT_APPS=( ufw ssh openssh-client curl git docker.io docker-compose apt-utils php-xml libc6 libstdc++6 python3-minimal ca-certificates tar openssh-server bash wget )
 
 # Remover potenciais bloqueios ao gerenciador de pacotes.
 sudo rm /var/lib/dpkg/lock-frontend
@@ -26,7 +28,7 @@ sudo ufw enable
 sudo systemctl enable docker
 
 # Criar container do VSCode.
-sudo docker run -d --name vscode -p 8443:8443 -v /home/$USER/.git:/.git -e DEFAULT_WORKSPACE=/.git linuxserver/code-server:latest
+sudo docker run -d --name vscode -p 8443:8443 -v /home/$USER/.git:/.git -e DEFAULT_WORKSPACE=/.git -e PASSWORD=$USER_PASS linuxserver/code-server:latest
 
 # Alterar propriedade do workspace.
 sudo chown $USER:$USER /home/$USER/.git -R
@@ -35,12 +37,13 @@ sudo chown $USER:$USER /home/$USER/.git -R
 sudo chmod 777 /home/$USER/.git -R
 
 # Configurar VSCode.
-wget https://raw.githubusercontent.com/paulogobetti/this-is-not-a-repository/main/vscode/settings.json -O /home/$USER/settings.json && sudo docker cp /home/$USER/settings.json vscode:/config/data/User/
+wget https://raw.githubusercontent.com/paulogobetti/this-is-not-a-repository/main/vscode/settings.json -O /home/$USER/settings.json
+
+sudo docker cp /home/$USER/settings.json vscode:/config/data/User/
 
 # Setar IP estático.
 
 # Configurar Git.
-# Pedir a senha com read.
 
-echo "Instalação finalizada com sucesso. VSCode em $LOCAL_IP:8443."
+echo "Instalação finalizada com sucesso. VSCode em $LOCAL_IP:8443, Senha: $USER_PASS."
 
