e8479b3e05fb23b944cba1386890fdabf715973c
Paulo Gobetti <78759327+paulogobetti@users.noreply.github.com>
2023-06-05 17:40:33 -0300

    Update

--- sudo.sh
+++ sudo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# wget https://raw.githubusercontent.com/paulogobetti/this-is-not-a-repository/main/sudo.sh && chmod +x sudo.sh && ./sudo.sh
+# wget https://raw.githubusercontent.com/paulogobetti/scripts/main/sudo.sh && chmod +x sudo.sh && ./sudo.sh
 #
 # # # #
 
