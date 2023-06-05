e1811c286b4a88e3dfdf6c923ae3abc3cd78e706
Paulo Gobetti <78759327+paulogobetti@users.noreply.github.com>
2023-06-05 17:39:23 -0300

    Update

--- debian-11-workspace-docker.sh
+++ debian-11-workspace-docker.sh
@@ -25,23 +25,30 @@ sudo apt install ufw ssh openssh-client curl git docker.io docker-compose apt-ut
 # Configurar e habilitar firewall.
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
-sudo ufw allow 22 && sudo ufw allow 80 && sudo ufw allow 8443
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 8443
 sudo ufw enable
 
 # Habilitar Docker na inicialização.
 sudo systemctl enable docker
 
+# Arquivo de configuração do VSCode.
+wget https://raw.githubusercontent.com/paulogobetti/scripts/main/vscode/settings.json -O /home/$USER/settings.json
+
 # Criar container do VSCode.
-sudo docker run -d --name vscode -p 8443:8443 -v /home/$USER/.git:/.git -e DEFAULT_WORKSPACE=/.git -e PASSWORD=$USER_PASS linuxserver/code-server:latest
+# Setar mesmo ID para todos os containers (para evitar problemas de permiossão).
+#sudo docker run -d --name vscode -p 8443:8443 -v /home/$USER/.git:/.git -e DEFAULT_WORKSPACE=/.git -e PASSWORD=$USER_PASS linuxserver/code-server:latest
+sudo docker run -d --restart always --name=vscode -e PUID=1000 -e PGID=1000 -e TZ=America/Sao_Paulo -e PASSWORD=$USER_PASS -e DEFAULT_WORKSPACE=/.workspace -p 8443:8443 -v /home/$USER/.workspace linuxserver/code-server:latest && sudo chown www-data:docker /home/$USER/.git -R && sudo docker cp /home/$USER/settings.json vscode:/config/data/User/
 
 # Alterar propriedade do workspace.
-sudo chown $USER:$USER /home/$USER/.git -R
+#sudo chown www-data:docker /home/$USER/.git -R
 
 # Alterar permissão do workspace.
-sudo chmod 777 /home/$USER/.git -R
+#sudo chmod 777 /home/$USER/.git -R
 
 # Configurar VSCode.
-wget https://raw.githubusercontent.com/paulogobetti/this-is-not-a-repository/main/vscode/settings.json -O /home/$USER/settings.json && sudo docker cp /home/$USER/settings.json vscode:/config/data/User/
+#sudo docker cp /home/$USER/settings.json vscode:/config/data/User/
 
 # Setar IP estático.
 
